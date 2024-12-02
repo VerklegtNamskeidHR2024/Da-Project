2fdde86d81d9b85b77cda676242cbc05d63c01c1
Hreimur Logi <70168436+Accent8@users.noreply.github.com>
2024-12-02 11:39:59 +0000

    made fixes

--- Model_Classes/model.py
+++ Model_Classes/model.py
@@ -7,15 +7,63 @@ class Human:
         self.system_permissions = system_permissions
         self.email = email
 
+    def set_name(self, name):
+        self.name = name
+    
+    def set_social_security_number(self, social_security_number):
+        self.social_security_number = social_security_number
+    
+    def set_phone_number(self, phone_number):
+        self.phone_number = phone_number
+
+    def set_location(self, location):
+        self.location = location
+
+    def set_system_permissions(self, system_permissions):
+        self.system_permissions = system_permissions
+    
+    def set_email(self, email):
+        self.email = email
+    
+    def get_name(self):
+        return self.name
+    
+    def get_social_security_number(self):
+        return self.social_security_number
+    
+    def get_phone_number(self):
+        return self.phone_number
+    
+    def get_location(self):
+        return self.location
+    
+    def get_system_permissions(self):
+        return self.system_permissions
+    
+    def get_email(self):
+        return self.email
+    
 class Manager(Human):
     def __init__(self, name, social_security_number, phone_number, location, system_permissions, email, staff_id, ):
         super().__init__(name, social_security_number, phone_number, location, system_permissions, email)
         self.staff_id = staff_id
+
+    def set_staff_id(self, staff_id):
+        self.staff_id = staff_id
+    
+    def get_staff_id(self):
+        return self.staff_id
         
 class Employee(Human):
     def __init__(self, name, social_security_number, phone_number, location, system_permissions, email, staff_id):
         super().__init__(name, social_security_number, phone_number, location, system_permissions, email)
         self.staff_id = staff_id
+
+    def set_staff_id(self, staff_id):
+        self.staff_id = staff_id
+
+    def get_staff_id(self):
+        return self.staff_id
 
 class Location:
     def __init__(self, country, location, airport, phone_number, branch_manager, opening_hours, amenities_list):
@@ -26,6 +74,31 @@ class Location:
         self.branch_manager = branch_manager
         self.opening_hours = opening_hours
         self.amenities_list = amenities_list
+
+    def set_country(self, country):
+        self.country = country
+
+    def set_location(self, location):
+        self.location = location
+    
+    def set_airport(self, airport):
+        self.airport = airport
+    
+    def set_phone_number(self, phone_number):
+        self.phone_number = phone_number
+
+    def set_branch_manager(self, branch_manager):
+        self.branch_manager = branch_manager
+
+    def set_opening_hours(self, opening_hours):
+        self.opening_hours = opening_hours
+
+    def set_amenities_list(self, amenities_list):
+        self.amenities_list = amenities_list
+    
+    def get_country(self):
+        return self.country
+    
         
 class Property:
     def __init__(self, name, location, total_price_to_fix):
@@ -50,7 +123,7 @@ class WorkRequest:
                 contractor_id, mark_as_done):
         self.work_request_id = work_request_id
         self.staff_id = staff_id
-        self.property_id = property
+        self.property_id = property_id
         self.date_of_creation = date_of_creation
         self.repetitive_work = repetitive_work
         self.reopen_interval = reopen_interval
@@ -67,7 +140,7 @@ class MaintenanceReport:
         self.report_id = report_id
         self.report_name = report_name
         self.location = location
-        self.property_id = property
+        self.property_id = property_id
         self.staff_id = staff_id
         self.regular_maintenance = regular_maintenance
         self.maintenance_description = maintenance_description
