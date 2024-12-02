7e702c2ce7afd7dce3126816320b00ca1527accc
kormakursnorra <kormakur02@gmail.com>
2024-12-02 16:48:32 +0000

    Changed names of input variables and instances, also moved some lines into set roles function

--- UI_Layer/start_menu_ui_layer.py
+++ UI_Layer/start_menu_ui_layer.py
@@ -1,6 +1,6 @@
 class StartMenu:
-    def __init__(self, log_in_choice: str, location_choice: str):
-        self.log_in_choice = log_in_choice
+    def __init__(self, role: str, location_choice: str):
+        self.role = role
         self.location_choice = location_choice    
 
     def show_ascii_nan_hq_monstrosity(self):
@@ -10,9 +10,9 @@ class StartMenu:
             print("{:>13}{:>12}{:>11}{:>5}{:>3}{:>10}{:>6}{:>4}".format("_\_(*)_/_", "___(*)___", ": : : :", "o o", "|", "| | |", "|", "_ ,"))
             print("{:0}{:>1}{:>31}".format("_______|-|_________/-\__________", ":", "_____|_|__|_____| | |_____| o-o"))
 
-    def show_roles_options(self):
-        log_in_choice = ""
-        while (log_in_choice != "q" and log_in_choice != "Q"):
+    def show_role_options(self):
+        role = ""
+        while (role != "q" and role != "Q"):
             print()
             print("Welcome to the NaN Air Properties and Staff System!")
             print("-" * 70)
@@ -28,18 +28,20 @@ class StartMenu:
             print("{:>20}{:>5}".format(">Quit System:", "q, Q"))
             print("-" * 70)
             
-            log_in_choice = input("Select an Option: ")
-            if log_in_choice == "1" or log_in_choice == "admin" or log_in_choice == "Admin": 
-                print()
-                print("{:<1}{:>3}{:>8}{:>7}{:>11}".format("1. Reykjavík", "|", "2. Nuuk", "|", "3. Kulusuk"))
-                print()
-                print("{:<1}{:>4}{:>12}{:>3}{:>16}".format("4. Þórshöfn", "|", "5. Tingwall", "|", "6. Longyearbyen"))
-                print()
-                location_choice = input("Select a location: ")
-            elif log_in_choice == "2" or log_in_choice == "manager" or log_in_choice == "Manager":
-                
-            elif log_in_choice == "3" or log_in_choice == "employee" or log_in_choice == "Employee":
             
-    def set_roles(): 
-
-    def set_locations():
+    def set_roles(self, role: str): 
+        role = input("Select an Option: ")
+        if role == "1" or role == "admin" or role == "Admin": 
+            
+        elif role == "2" or role == "manager" or role == "Manager":
+            
+        elif role == "3" or role == "employee" or role == "Employee":
+    
+    def set_locations(self, location_choice: str):
+        print()
+        print("{:0}{:>3}{:>8}{:>7}{:>11}".format("1. Reykjavík", "|", "2. Nuuk", "|", "3. Kulusuk"))
+        print()
+        print("{:0}{:>4}{:>12}{:>3}{:>16}".format("4. Þórshöfn", "|", "5. Tingwall", "|", "6. Longyearbyen"))
+        print()
+        location_choice = input("Select a location: ")
+         
