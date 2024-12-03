052ef0492ff7061bfc810c98edd7335e7b2baec6
DanielaTebe <danielafannars@gmail.com>
2024-12-03 12:55:41 +0000

    bjo til toflu

--- UI_Layer/property_ui_layer.py
+++ UI_Layer/property_ui_layer.py
@@ -1,3 +1,39 @@
 class property_UI_menu:
-    def __init__(self,logic_wrapper):
+    def __init__(self, logic_wrapper, rank, location):
         self.logic_wrapper = logic_wrapper
+        self.rank = rank
+        self.location = location
+
+
+
+    def display_all_properties():
+        print()
+        print("Properties")
+        print("-" * 70)
+        print("Nr |Address | Name | ID")
+        print("-" * 70)
+        print("1 | Karfavogur 5 | Hús 1 | 20")
+        print("2 | Karfavogur 6 | Hús 2 | 21") 
+        print()
+        print("1. Select Property")
+        print("2. Add Property")
+        print("-" * 70)
+        property_options = int(input("Select an Option: "))
+
+    def display_selected_property():
+        print()
+        print("Property Information")
+        print("-" * 70)
+        print("Name: jehf")
+        print("Address: jfsab")
+        print("Location: fskjf")
+        print("ID: fdsv")
+        print("Price: dfgg")
+        print("Condition: gdsf")
+        print("-" * 70)
+        print("1. Select Property")
+        print("2. Add Property")
+        
+
+
+
