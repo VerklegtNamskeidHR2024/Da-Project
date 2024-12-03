430ce770b3707467dfc1e3e542bfa51838f00043
Yeabsira05 <tesfyabsera18@gmail.com>
2024-12-03 16:25:21 +0000

    update

--- UI_Layer/maintenance_report_ui_layer.py
+++ UI_Layer/maintenance_report_ui_layer.py
@@ -2,20 +2,7 @@ class maintenance_report_UI_menu:
     def __init__(self,logic_wrapper):
         self.logic_wrapper = logic_wrapper
     
-    def display_all_open_work_requests(self):
-        """Prints out all open work requests with their ID, Name and Description """
-        
-        print()
-        print("Work Request Menu")
-        print("-" * 70)
-        print()
-        print("{:>20}".format("[ Open and Upcoming Work Requests ]"))
-        print("{:0}{:>3}{:>8}{:>7}{:>11}".format("ID", "|", "Name", "|", "Description"))
-        print("-" * 70)
-        work_request_list = self.logic_wrapper.get_all_work_requests()
-        for item in work_request_list:
-            print("{:0}{:>3}{:>8}{:>7}{:>11}".format(
-                work_request_list["work_request_id"], 
-                "|", work_request_list["name"], 
-                "|", work_request_list["description"]
-                ))
+    def display_maintenance_report(self):
+     
+        print("1. Select Property")
+        print("2. Add Property")
