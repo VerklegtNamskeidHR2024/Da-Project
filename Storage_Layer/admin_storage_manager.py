4d21a076b51182189d3f9e489e967432b7a40447
Yeabsira05 <tesfyabsera18@gmail.com>
2024-12-03 10:51:14 +0000

    message

--- Storage_Layer/admin_storage_manager.py
+++ Storage_Layer/admin_storage_manager.py
@@ -1,1 +1,13 @@
- 
+class AdminStorage:
+    def __init__(self, admin, storage):
+        self.admin = admin
+        self.storage = storage 
+
+    def add_user(self, user_id, user_data):
+        #add a user to storage
+        pass
+
+    def get_user(self, user_id):
+        #get a user from storage
+        pass
+
