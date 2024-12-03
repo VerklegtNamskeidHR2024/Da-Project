aa2447fe5c36cd5c3494d6319b50c6b8dbce72ab
kormakursnorra <kormakur02@gmail.com>
2024-12-03 12:21:24 +0000

    changed self.rank = ... to return ... for all match case

--- UI_Layer/start_menu_ui_layer.py
+++ UI_Layer/start_menu_ui_layer.py
@@ -26,7 +26,14 @@ class Main_Menu:
         self.select_location_for_system()
         self.display_menu_items()
     
-    def show_login_choice_for_system(self):
+    def show_ascii_art_hq(self):
+        print("{:>61}".format("==================="))
+        print("{:>44}{:>13}{:>3}".format("|", "NaN Air HQ", "|"))
+        print("{:>14}{:>7}{:>15}{:>8}{:>10}{:>6}".format("___________", ".", ": : : :", "|", "_____","|"))
+        print("{:>13}{:>12}{:>11}{:>5}{:>3}{:>10}{:>6}{:>4}".format("_\_(*)_/_", "___(*)___", ": : : :", "o o", "|", "| | |", "|", "_ ,"))
+        print("{:0}{:>1}{:>31}".format("_______|-|_________/-\__________", ":", "_____|_|__|_____| | |_____| o-o"))
+
+    def select_user_for_system(self):
         print()
         print("Welcome to the NaN Air Properties and Staff System!")
         print("-" * 70)
@@ -41,30 +48,14 @@ class Main_Menu:
         print("{:>24}{:>5}".format(">Go to Home Page:", "home, Home"))
         print("{:>20}{:>5}".format(">Quit System:", "q, Q"))
         print("-" * 70)
-
-    def show_ascii_art_hq(self):
-        print("{:>61}".format("==================="))
-        print("{:>44}{:>13}{:>3}".format("|", "NaN Air HQ", "|"))
-        print("{:>14}{:>7}{:>15}{:>8}{:>10}{:>6}".format("___________", ".", ": : : :", "|", "_____","|"))
-        print("{:>13}{:>12}{:>11}{:>5}{:>3}{:>10}{:>6}{:>4}".format("_\_(*)_/_", "___(*)___", ": : : :", "o o", "|", "| | |", "|", "_ ,"))
-        print("{:0}{:>1}{:>31}".format("_______|-|_________/-\__________", ":", "_____|_|__|_____| | |_____| o-o"))
-
-    def show_location_choice_for_system(self):
-        print()
-        print("{:0}{:>3}{:>8}{:>7}{:>11}".format("1. Reykjavik", "|", "2. Nuuk", "|", "3. Kulusuk"))
-        print()
-        print("{:0}{:>4}{:>12}{:>3}{:>16}".format("4. Torshavn", "|", "5. Tingwall", "|", "6. Longyearbyen"))
-        print()
-    
-    def log_in_choice(self):
         selected_user_login_choice = input("Select an Option: ")
         match selected_user_login_choice:
             case "1":
-                self.rank = "Admin"
+                return "Admin"
             case "2":
-                self.rank = "Manager"
+                return "Manager"
             case "3":
-                self.rank = "Employee"
+                return "Employee"
             case "q": 
                 print("Departing from NaN Air, Thank you for Visiting!")
                 pass
@@ -73,23 +64,28 @@ class Main_Menu:
                 pass
             case _:
                 print("No User Found, Please Try Again.")
-                self.show_login_choice_for_system()
-        
-    def location_choice(self):
+                self.select_user_for_system()
+
+    def select_location_for_system(self):
+        print()
+        print("{:0}{:>3}{:>8}{:>7}{:>11}".format("1. Reykjavik", "|", "2. Nuuk", "|", "3. Kulusuk"))
+        print()
+        print("{:0}{:>4}{:>12}{:>3}{:>16}".format("4. Torshavn", "|", "5. Tingwall", "|", "6. Longyearbyen"))
+        print()
         selected_location_choice = input("Select a Location: ")
         match selected_location_choice:
             case "1":
-                self.location = "Reykjavik"
+                return "Reykjavik"
             case "2":
-                self.location = "Nuuk"
+                return "Nuuk"
             case "3":
-                self.location = "Kulusuk"
+                return "Kulusuk"
             case "4":
-                self.location = "Torshavn"
+                return "Torshavn"
             case "5":
-                self.location = "Tingwall"
+                return "Tingwall"
             case "6":
-                self.location = "Longyearbyen"
+                return "Longyearbyen"
             case "q": 
                 print("Departing from NaN Air, Thank you for Visiting!")
                 pass
@@ -98,8 +94,8 @@ class Main_Menu:
                 pass
             case _:
                 print("No Location Found, Please Try Again.")
-                self.show_location_choice_for_system()
-
+                self.select_location_for_system()
+        
     def display_menu_items(self):
         print(" Manager - Home Page")
         print("-" * 70)
