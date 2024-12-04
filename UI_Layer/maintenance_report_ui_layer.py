bdfcf8fd99419c2b6d315b78faab76dbfcf5f99a
Yeabsira05 <tesfyabsera18@gmail.com>
2024-12-04 11:02:08 +0000

    changes

--- UI_Layer/maintenance_report_ui_layer.py
+++ UI_Layer/maintenance_report_ui_layer.py
@@ -1,4 +1,4 @@
-class MaintenanceReportUIMenu:
+class maintenance_report_UI_menu:
     def __init__(self, logic_wrapper, rank, location):
         self.logic_wrapper = logic_wrapper
         self.rank = rank
