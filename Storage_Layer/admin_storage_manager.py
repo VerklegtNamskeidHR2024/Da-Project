d18cc1e2a5b91200d46f4b0d4cbfbf30c7057f24
Hreimur Logi <70168436+Accent8@users.noreply.github.com>
2024-12-04 13:49:49 +0000

    added write to file for all storage mananger

--- Storage_Layer/admin_storage_manager.py
+++ Storage_Layer/admin_storage_manager.py
@@ -4,18 +4,14 @@ from Model_Classes.admin_model import Admin
 class admin_storage:
     def __init__(self):
         pass
-    def add_admin(self):
-        pass
-    def get_all_admin(self):
+
+    def get_all_admin(self) -> list[Admin]:
         with open('Data/admin_storage.json', 'r') as admin_file:
             admin_data = json.load(admin_file)
         admin_list = [Admin(**data) for data in admin_data]
         return admin_list
     
-    def write_to_file_admin(self, list_of_admins):
+    def write_to_file_admin(self, list_of_admins: list[Admin]):
         dict_of_admins = [admin.to_dict() for admin in list_of_admins]
         with open('Data/admin_storage.json', 'w') as admin_file:
             json.dump(dict_of_admins, admin_file, indent=4)
-
-    def set_id_and_add_to_storage(self):
-        pass
