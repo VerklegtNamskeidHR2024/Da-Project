2b60670fe0e9af162320f6805b6ae5f6bff202de
kormakursnorra <kormakur02@gmail.com>
2024-12-06 13:57:19 +0000

    fixed the sanity check for work request name, description, id and property id

--- Logic_Layer/work_request_logic_manager.py
+++ Logic_Layer/work_request_logic_manager.py
@@ -5,17 +5,20 @@ class work_request_logic_manager:
 
     def sanity_check_work_request_id(self, work_request_id: str) -> bool:
         work_request_id_list = list(work_request_id)
-        if (len(work_request_id_list) == 3 and work_request_id_list[0] == "W" and work_request_id_list[1] == "R") == True:
+        is_number = (work_request_id_list[2]).isalpha()
+        if (len(work_request_id_list) == 3 and work_request_id_list[0] == "W" and 
+            work_request_id_list[1] == "R" and is_number == False) == True:
             return True
         else:
-            return False  
+            return print("whut")  
 
     def sanity_check_new_work_request_property_id(self, property_id: str) -> bool:
         property_id_listed = list(property_id)
-        if (len(property_id_listed) == 2 and property_id_listed[0] == "P") == True:
+        is_number = (property_id_listed[1]).isalpha()
+        if (len(property_id_listed) == 2 and property_id_listed[0] == "P" and is_number == False) == True:
             return True
         else:
-            return False 
+            return print("lol") 
     
     def sanity_check_boolean_input_work_requests(self, yes_or_no: str) -> bool:
         match yes_or_no:
@@ -53,17 +56,14 @@ class work_request_logic_manager:
                 highest_id = int(stripped_id)
         highest_id += 1
         new_work_request_id = "WR" + str(highest_id)
-        print(new_work_request_id)
         Work_request.set_work_request_id(new_work_request_id)
         return Work_request
             
 
     def add_work_request(self, Work_request: object) -> bool:
         Work_request_with_id = self.set_new_work_request_id(Work_request)
-        print(Work_request_with_id)
         all_works_requests = self.Storage_Layer_Wrapper.get_all_work_requests()
         all_works_requests.append(Work_request_with_id)
-        print(all_works_requests)
         self.Storage_Layer_Wrapper.write_to_file_work_requests(all_works_requests) 
         return print("Work Request Has Been Created")
 
@@ -80,6 +80,10 @@ class work_request_logic_manager:
             if work_request.location == location and work_request.work_request_id == work_request_id:
                 return work_request
         return 
+    
+    def get_all_work_requests(self, rank: str, location: str) -> list:
+        all_work_requests = self.Storage_Layer_Wrapper.get_all_work_requests()
+        return all_work_requests
 
     def get_all_work_requests_at_location(self, rank, location) -> list:
             '''Gets all work requests at specific location'''
