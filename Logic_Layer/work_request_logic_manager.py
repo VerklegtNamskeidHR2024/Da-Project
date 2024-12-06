e2bbf82616169afe68380cf456e425382b97eff2
kormakursnorra <kormakur02@gmail.com>
2024-12-06 13:36:01 +0000

    Done with the create new work request function, need to fix the sanity check for the inputs

--- Logic_Layer/work_request_logic_manager.py
+++ Logic_Layer/work_request_logic_manager.py
@@ -5,28 +5,46 @@ class work_request_logic_manager:
 
     def sanity_check_work_request_id(self, work_request_id: str) -> bool:
         work_request_id_list = list(work_request_id)
-        if (len(work_request_id_list) == 5 and work_request_id_list[0] == "W" and work_request_id_list[1] == "R") == True:
+        if (len(work_request_id_list) == 3 and work_request_id_list[0] == "W" and work_request_id_list[1] == "R") == True:
             return True
         else:
             return False  
 
     def sanity_check_new_work_request_property_id(self, property_id: str) -> bool:
         property_id_listed = list(property_id)
-        if (len(property_id_listed) == 5 and property_id_listed[0] == "P") == True:
+        if (len(property_id_listed) == 2 and property_id_listed[0] == "P") == True:
             return True
         else:
             return False 
     
-    def sanity_check_repititive_work_requests(self, yes_or_no: str) -> bool:
+    def sanity_check_boolean_input_work_requests(self, yes_or_no: str) -> bool:
         match yes_or_no:
             case "yes" | "Yes":
                 return True
             case "no" | "No":
-                return False
+                return True
             case _:
-                return 
-    
-    def set_new_work_request(self, rank, location, Work_request: object=None) -> str:
+                return print("Try Again.")
+
+    def sanity_check_priority_for_request(self, priority: str) -> bool:
+        match priority:
+            case "high" | "High":
+                return True
+            case "medium" | "Medium":
+                return True
+            case "low" | "Low":
+                return True
+            case _ :
+                return print("Invalid dumb dumb")
+
+    def sanity_check_location_for_request(self, set_location: str) -> bool:
+        valid_locations = ["Reykjavik", "Nuuk", "Kulusuk", "Longyearbyen", "Torshavn", "Tingwall"]
+        for location in valid_locations:
+            if location == set_location:
+                return True
+        return print("Try Again Pookie.")
+
+    def set_new_work_request_id(self, Work_request: object) -> str:
         highest_id = 0
         all_work_requests = self.Storage_Layer_Wrapper.get_all_work_requests()
         for work_request in all_work_requests:
@@ -35,16 +53,19 @@ class work_request_logic_manager:
                 highest_id = int(stripped_id)
         highest_id += 1
         new_work_request_id = "WR" + str(highest_id)
-        Work_request_with_id = Work_request.set_work_request_id(new_work_request_id)
-        return Work_request_with_id
+        print(new_work_request_id)
+        Work_request.set_work_request_id(new_work_request_id)
+        return Work_request
             
 
-    def add_work_request(self, rank, location, Work_request: object=None) -> bool:
-        Work_request_with_id = self.set_new_work_request(Work_request)
+    def add_work_request(self, Work_request: object) -> bool:
+        Work_request_with_id = self.set_new_work_request_id(Work_request)
+        print(Work_request_with_id)
         all_works_requests = self.Storage_Layer_Wrapper.get_all_work_requests()
         all_works_requests.append(Work_request_with_id)
+        print(all_works_requests)
         self.Storage_Layer_Wrapper.write_to_file_work_requests(all_works_requests) 
-        return True
+        return print("Work Request Has Been Created")
 
 
     def edit_work_request(self, Work_request: object) -> bool:
@@ -59,7 +80,7 @@ class work_request_logic_manager:
             if work_request.location == location and work_request.work_request_id == work_request_id:
                 return work_request
         return 
-    
+
     def get_all_work_requests_at_location(self, rank, location) -> list:
             '''Gets all work requests at specific location'''
             work_request_sorted_list = []
